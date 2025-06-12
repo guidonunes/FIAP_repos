ce01229bff56ad5b35af45988df19223ba1b7360
Guilherme Nunes <guilherme.augd@gmail.com>
2025-06-12 11:42:02 -0300

    string exercise final

---
+++ cap10/js_string/script.js
@@ -0,0 +1,23 @@
+let phrase = "Hello, World!";
+
+
+
+
+console.log(phrase);
+console.log(phrase.length);
+console.log(phrase[0]);
+console.log(phrase.toUpperCase());
+console.log(phrase.toLowerCase());
+console.log(phrase.charAt(4));
+console.log(phrase.indexOf("W"));
+console.log(phrase.lastIndexOf("o"));
+console.log(phrase.includes("e"));
+console.log(phrase.concat(" How are you?"));
+console.log(phrase.replace("World", "Universe"));
+console.log(phrase.substring(2, 10));
+console.log(phrase.slice(7, 12));
+console.log(phrase.split(""));
+console.log(phrase.split("e"));
+console.log(phrase.trim());
+console.log(phrase.startsWith("z"));
+console.log(phrase.endsWith("!"));
