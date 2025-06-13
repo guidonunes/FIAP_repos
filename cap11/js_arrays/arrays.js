89c0b1c174de54983d07c180f58a81b3f4350eab
Guilherme Nunes <guilherme.augd@gmail.com>
2025-06-13 16:59:18 -0300

    arrays exercise

---
+++ cap11/js_arrays/arrays.js
@@ -0,0 +1,18 @@
+const arr = new Array("a", "b", "c", "d", "e");
+
+console.log(arr);
+console.log(arr[0]); // Accessing the first element
+console.log(arr[1]); // Accessing the second element
+console.log(arr[2]); // Accessing the third element
+console.log(arr[3]); // Accessing the fourth element
+console.log(arr[4]); // Accessing the fifth element
+
+
+const heros = ["Batman", "Superman", "Wonder Woman", "Flash", "Aquaman"];
+
+console.log(heros);
+console.log(heros[0]);
+console.table(heros); // Displaying the array in a table format
+
+
+console.log(arr.length); // Getting the length of the array
